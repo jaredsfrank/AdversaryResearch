dc0f4cb1a5940d62ccdb046e09e4de4e69fa9423
Jared Frank <jaredfrank@dhcp-rhodes-2884.redrover.cornell.edu>
2017-09-14 19:05:23 -0400

    non targeted

--- adversaries.py
+++ adversaries.py
@@ -95,7 +95,7 @@ class LBFGS(object):
       opt = optim.SGD(test(inputs), lr=lr, momentum=0.9)
       self.clamp_images(images)
       old_images = images.clone()
-      new_labels = torch.topk(resnet(inputs), 2, 1)[1]
+      new_labels = torch.topk(resnet(inputs), 2, 1)[1][:, 1]
       # new_labels = Variable(torch.LongTensor([target_class]*batch_size)).cuda()
       print new_labels
       return
