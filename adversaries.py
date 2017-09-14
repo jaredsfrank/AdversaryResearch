cadfa541784fb665bcedaa28f7d10501a678ceb8
Jared Frank <jaredfrank@dhcp-gs-5796.redrover.cornell.edu>
2017-09-14 12:23:55 -0400

    printing mse

--- adversaries.py
+++ adversaries.py
@@ -110,10 +110,11 @@ class LBFGS(object):
           print outputs.data[:, target_class] - predicted[0]
         predicted = predicted[1]
         iters += 1
-        if self.verbose and not self.all_changed(original_labels, predicted): # self.is_done(predicted, target_class, batch_size, iters, min_iters):
-            self.save_figure(inputs.data, "After_{}_{}".format(image_reg, lr))
-            self.diff(images, old_images)
-            plt.show()
+        if not self.all_changed(original_labels, predicted): # self.is_done(predicted, target_class, batch_size, iters, min_iters):
+            if self.verbose:
+              self.save_figure(inputs.data, "After_{}_{}".format(image_reg, lr))
+              self.diff(images, old_images)
+              plt.show()
         else:
             loss.backward()
             opt.step()
