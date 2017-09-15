afd7b7456080ffd3bfdb4abb398423813cc4dce0
Jared Frank <jaredfrank@dhcp-gs-1846.redrover.cornell.edu>
2017-09-15 15:36:20 -0400

    debugging

--- adversaries.py
+++ adversaries.py
@@ -77,7 +77,7 @@ class LBFGS(object):
     else:
       np_orig = original_labels.numpy()
       np_preds = predictions.numpy()
-    print np_orig != np_preds
+    print original_labels
     return np.all(np_orig != np_preds)
 
   def clamp_images(self, images):
