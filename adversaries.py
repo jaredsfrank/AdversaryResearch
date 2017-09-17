1e6fa3f5998a809897a753daffefd5302e134cbb
Jared Frank <jaredfrank@dhcp-rhodes-958.redrover.cornell.edu>
2017-09-17 15:10:29 -0400

    only updating labels that are correctly labeled

--- adversaries.py
+++ adversaries.py
@@ -99,12 +99,12 @@ class LBFGS(object):
       predicted_classes = torch.max(outputs.data, 1)[1]
       print predicted_classes
       print original_labels
-      predicting_correct_class = byte(predicted_classes == original_labels)
+      predicting_correct_class = torch.byte(predicted_classes == original_labels)
       second_best_class = torch.topk(outputs, 2, 1)[1][:, 1]
       # For each label in outputs that is correctly classified, replace
       # use second best class. Otherwise, stick with current prediction
       new_labels = outputs.masked_scatter_(predicting_correct_class,
-                                                   second_best_class)
+                                           second_best_class)
 
     else:
       new_labels = Variable(torch.LongTensor([target_class]*self.batch_size))
