405771610fd74496386601ef14396c94aeb3713d
Jared Frank <jaredfrank@dhcp-hol-4555.redrover.cornell.edu>
2017-09-17 20:53:02 -0400

    added fast gradient descent method

---
+++ lbfgs_main.py
@@ -0,0 +1,51 @@
+"""Runs adversarial example trainer
+
+Example Usage:
+python lbfgs_main.py --batch_size 100 \
+    --target_class 1 --image_reg 100 --lr 1
+
+
+"""
+import argparse
+import model_testing
+import LBFGS
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", 
+    help="Number of adversarial examples to generate",
+    type=int)
+parser.add_argument("--target_class",
+    help="Target class",
+    type=int)
+parser.add_argument("--image_reg",
+    help="Regularizer on the image loss function",
+    type=int)
+parser.add_argument("--lr",
+    help="Learning rate", 
+    type=float)
+parser.add_argument("--max_iters",
+    help="Maximum number of iterations", 
+    type=float)
+parser.add_argument('--verbose', action='store_true', help='print messages?')
+parser.add_argument('--show_images', action='store_true', help='show images?')
+parser.add_argument('--cuda', action='store_true', help='use cuda?')
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    lbfgs = LBFGS.LBFGS(args.batch_size, args.lr)
+    ave_mse = 0.0
+    iters = 10
+    if args.verbose:
+        lbfgs.verbose = True
+    if args.show_images:
+        lbfgs.show_images = True
+    if args.cuda:
+        lbfgs.cuda = True
+    if args.max_iters is not None:
+        lbfgs.max_iters = args.max_iters
+
+    ave_mse = lbfgs.create_all_adversaries(target_class=args.target_class,
+                                           image_reg=args.image_reg)
+    print ave_mse
+    
