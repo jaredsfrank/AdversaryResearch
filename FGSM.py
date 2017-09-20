3e751968f83096f661201ae80966cb368cee6dc1
Jared Frank <jaredfrank@dhcp-rhodes-718.redrover.cornell.edu>
2017-09-19 23:49:52 -0400

    moved to l inf

--- FGSM.py
+++ FGSM.py
@@ -53,6 +53,7 @@ class FGSM(adversaries.Adverarial_Base):
     self.clamp_images(images)
     outputs = model(inputs)
     predicted_loss, predicted_classes = torch.max(outputs.data, 1)
-    return 1, torch.max(images - old_images), self.percent_changed(original_labels, predicted_classes)
+    print images.shape
+    return 1, torch.Tensor(torch.max(images - old_images)), self.percent_changed(original_labels, predicted_classes)
 
 
