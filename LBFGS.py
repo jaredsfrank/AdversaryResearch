11558b613c7de8569ae068730c78a88fc7580763
Jared Frank <jaredfrank@dhcp-rhodes-718.redrover.cornell.edu>
2017-09-20 00:07:57 -0400

    moved to l inf

--- LBFGS.py
+++ LBFGS.py
@@ -82,5 +82,5 @@ class LBFGS(adversaries.Adverarial_Base):
           new_labels = self.target_class_tensor(target_class, outputs, original_labels)
           # self.MSE(images, Variable(old_images))
     print images.shape[0]
-    print ((images - old_images).cpu().numpy().reshape(images.shape[0],-1).max(0)).shape
+    print ((images - old_images).cpu().numpy().reshape(images.shape[0],-1).max(1)).shape
     return iters, torch.max(images - old_images), self.percent_changed(original_labels, predicted_classes)
