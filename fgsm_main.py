b65717091af9b3045a2c341251b9515e7126bca9
Jared Frank <jaredfrank@dhcp-rhodes-1348.redrover.cornell.edu>
2017-09-20 14:19:47 -0400

    new range

--- fgsm_main.py
+++ fgsm_main.py
@@ -29,6 +29,21 @@ parser.add_argument('--show_images', action='store_true', help='show images?')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
 args = parser.parse_args()
 
+def better_range(min_value, max_value):
+    l = [min_value, max_value]
+    q = Queue.Queue()
+    q.put((min_value+1, max_value))
+    seen = set()
+    while not q.empty():
+        next_min, next_max = q.get()
+        next_mid = (next_min + next_max)/2
+        l.append(next_mid)
+        if next_max - next_min > 1:
+            if next_mid - next_min > 0:
+                q.put((next_min, next_mid))
+            if next_max - next_mid > 1:
+                q.put((next_mid+1, next_max))
+    return l
 
 if __name__ == "__main__":
     fgsm = FGSM.FGSM(args.batch_size)
@@ -41,15 +56,12 @@ if __name__ == "__main__":
     if args.cuda:
         fgsm.cuda = True
 
-    fgsm.create_all_adversaries(target_class=args.target_class,
-                                image_reg=args.image_reg,
-                                lr=args.lr)
     save_result = []
     np.savetxt("/scratch/jsf239/fgsm_results.csv", np.array(save_result))
-    for i in np.arange(0.0001, .1, .001):
+    for i in better_range(1, 9):
         ave_mse, succ = fgsm.create_all_adversaries(target_class=args.target_class,
                                                    image_reg=args.image_reg,
-                                                   lr=i)
+                                                   lr=(3**i * 10**-4))
         save_result.append([ave_mse, succ])
         np.savetxt("/scratch/jsf239/fgsm_results.csv", np.array(save_result))
     print ave_mse
