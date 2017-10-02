f3f06948e814f4971bc1bef2c456971105ca66e9
Jared Frank <jaredfrank@dhcp-rhodes-3291.redrover.cornell.edu>
2017-10-02 16:46:09 -0400

    trying bayesian opt

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -77,7 +77,7 @@ def evaluate_model(model):
 if __name__ == '__main__':
 	x_data = [0.1]
 	for i in range(10):
-		train_x = Variable(torch.Tensor(x_data))
+		train_x = Variable(torch.Tensor(np.array(x_data)))
 		train_y = Variable(torch.sin(train_x.data * (2 * math.pi)) + torch.randn(train_x.size()) * 0.2)
 		model = train_model(train_x, train_y)
 		evaluate_model(model)
