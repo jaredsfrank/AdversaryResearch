0e64feeb8b33f912036f056116e3799d888354e0
Jared Frank <jaredfrank@dhcp-rhodes-2616.eduroam.cornell.edu>
2017-10-02 23:32:48 -0400

    using bayes opt package

--- bayes_opt/helpers.py
+++ bayes_opt/helpers.py
@@ -37,15 +37,18 @@ def acq_max(ac, gp, y_max, bounds):
     """
 
     # Warm up with random points
+    print("1")
     x_tries = np.random.uniform(bounds[:, 0], bounds[:, 1],
                                  size=(100000, bounds.shape[0]))
     ys = ac(x_tries, gp=gp, y_max=y_max)
     x_max = x_tries[ys.argmax()]
     max_acq = ys.max()
+    print("2")
 
     # Explore the parameter space more throughly
     x_seeds = np.random.uniform(bounds[:, 0], bounds[:, 1],
                                 size=(250, bounds.shape[0]))
+    print("3")
     for x_try in x_seeds:
         # Find the minimum of minus the acquisition function
         res = minimize(lambda x: -ac(x.reshape(1, -1), gp=gp, y_max=y_max),
@@ -57,6 +60,7 @@ def acq_max(ac, gp, y_max, bounds):
         if max_acq is None or -res.fun[0] >= max_acq:
             x_max = res.x
             max_acq = -res.fun[0]
+    print("4")
 
     # Clip output to make sure it lies within the bounds. Due to floating
     # point technicalities this is not always the case.
@@ -94,6 +98,8 @@ class UtilityFunction(object):
 
     @staticmethod
     def _ucb(x, gp, kappa):
+        print("am i in here?")
+        print(x)
         results = gp(Variable(torch.Tensor(x)))
         mean, std = results.std(), results.mean()
         return mean + kappa * std
