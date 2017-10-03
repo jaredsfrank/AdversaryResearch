388adf9f3a0c877a75bfe9df3b8e183d96c3c34f
Jared Frank <jaredfrank@dhcp-rhodes-2191.redrover.cornell.edu>
2017-10-03 16:47:10 -0400

    using bayes opt package

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -35,7 +35,7 @@ def plot_model_and_predictions(model, train_x, train_y, plot_train_data=True):
         ax.plot(train_x.data.numpy(), train_y.data.numpy(), 'k*')
         ax.plot(test_x.data.numpy(), rand_var.mean().data.numpy(), 'b')
         ax.fill_between(test_x.data.numpy(), lower.data.numpy(), upper.data.numpy(), alpha=0.5)
-        ax.set_ylim([-200, 200])
+        ax.set_ylim([-1, 1])
         ax.legend(['Observed Data', 'Mean', 'Confidence'])
         ax.set_title(title)
 
