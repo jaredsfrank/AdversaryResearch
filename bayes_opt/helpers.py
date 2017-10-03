e4f711ce5220150157bd95a8461a9976e766959e
Jared Frank <jaredfrank@dhcp-rhodes-2616.eduroam.cornell.edu>
2017-10-02 23:34:09 -0400

    using bayes opt package

--- bayes_opt/helpers.py
+++ bayes_opt/helpers.py
@@ -39,7 +39,7 @@ def acq_max(ac, gp, y_max, bounds):
     # Warm up with random points
     print("1")
     x_tries = np.random.uniform(bounds[:, 0], bounds[:, 1],
-                                 size=(100000, bounds.shape[0]))
+                                 size=(1000, bounds.shape[0]))
     ys = ac(x_tries, gp=gp, y_max=y_max)
     x_max = x_tries[ys.argmax()]
     max_acq = ys.max()
