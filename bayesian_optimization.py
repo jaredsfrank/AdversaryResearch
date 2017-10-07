809c1809868a0af608a61b21cabcc460aee197ef
Jared Frank <jaredfrank@Jareds-MacBook-Pro.local>
2017-10-07 17:52:30 -0400

    added more log noise

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -15,7 +15,7 @@ from bayes_opt.helpers import UtilityFunction, acq_max
 class ExactGPModel(gpytorch.GPModel):
     def __init__(self):
         # The log_noise_bounds add a random constant to the covariance matrix diagonal
-        super(ExactGPModel,self).__init__(GaussianLikelihood(log_noise_bounds=(0, 5)))
+        super(ExactGPModel,self).__init__(GaussianLikelihood(log_noise_bounds=(1, 1000)))
         self.mean_module = ConstantMean()# constant_bounds=(-1, 1))
         self.covar_module = RBFKernel()# log_lengthscale_bounds=(-5, 5))
     
