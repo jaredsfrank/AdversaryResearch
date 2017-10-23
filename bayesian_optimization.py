41f2714523a2def90fc26f3dd51470883169b129
Jared Frank <jaredfrank@dhcp-rhodes-509.redrover.cornell.edu>
2017-10-22 20:33:38 -0400

    added bayesian optimization

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,18 +1,5 @@
 """
 Runs bayesian optimization on a function
-
-NOTE:
-    Currently I am using this file to just test hyperparams for the GP Regression
-    Therefore it currently takes parameters that correspond to some of the tunable hyperparameters
-    Example Run:
-        python bayesian_optimization.py -100 100 -100 100 1 4
-        Corresponds to running with:
-            GaussianLiklihood log_noise_bounds (-100, 100)
-            mean_module (-100, 199)
-            constant_boundslog_lengthscale_bounds (1, 4)
-
-
-
 """
 
 
@@ -28,118 +15,82 @@ from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.random_variables import GaussianRandomVariable
 from torch.autograd import Variable
 from bayes_opt.helpers import UtilityFunction, acq_max
-import argparse
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("log_noise_min", 
-                    help="Minimum of Log Noise",
-                    type=float)
-parser.add_argument("log_noise_max", 
-                    help="Maximum of Log Noise",
-                    type=float)
-parser.add_argument("const_min", 
-                    help="Constant Bounds Minimum",
-                    type=float)
-parser.add_argument("const_max", 
-                    help="Constant Bounds Maximum",
-                    type=float)
-parser.add_argument("cov_min", 
-                    help="Covariance log lengthscale minimum",
-                    type=float)
-parser.add_argument("cov_max", 
-                    help="Covariance log lengthscale maximum",
-                    type=float)
-args = parser.parse_args()
 
 
 class ExactGPModel(gpytorch.GPModel):
-    def __init__(self, log_noise_min, log_noise_max, constant_mean_min, constant_noise_max, log_lengthscale_min, log_lengthscale_max):
-        # The log_noise_bounds add a random constant to the covariance matrix diagonal
-        super(ExactGPModel,self).__init__(GaussianLikelihood(log_noise_bounds=(log_noise_min, log_noise_max)))
-        self.mean_module = ConstantMean(constant_bounds=(constant_mean_min, constant_noise_max))
-        self.covar_module = RBFKernel(log_lengthscale_bounds=(log_lengthscale_min, log_lengthscale_max))
+    def __init__(self):
+        super(ExactGPModel,self).__init__(GaussianLikelihood(log_noise_bounds=(-5, 5)))
+        self.mean_module = ConstantMean(constant_bounds=(-1, 1))
+        self.covar_module = RBFKernel(log_lengthscale_bounds=(-5, 5))
     
     def forward(self,x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return GaussianRandomVariable(mean_x, covar_x)
 
-def plot_model_and_predictions(model, train_x, train_y, plot_train_data=True):
-    f, observed_ax = plt.subplots(1, 1, figsize=(8, 8))
-    test_x = Variable(torch.linspace(-5, 5, 51))
-    observed_pred = model(test_x)
+class BayesOpt(object):
 
-    def ax_plot(ax, rand_var, title):
-        lower, upper = rand_var.confidence_region()
-        # if plot_train_data:
-        print (train_x, train_y)
-        ax.plot(train_x.data.numpy(), train_y.data.numpy(), 'k*')
-        ax.plot(test_x.data.numpy(), rand_var.mean().data.numpy(), 'b')
-        ax.fill_between(test_x.data.numpy(), lower.data.numpy(), upper.data.numpy(), alpha=0.5)
-        ax.set_ylim([-200, 200])
-        ax.legend(['Observed Data', 'Mean', 'Confidence'])
-        ax.set_title(title)
+    def self.__init__(self, eval_function, bounds=[[0,1]], initial_points=30):
+        self.train_x = []
+        self.train_y = []
+        self.eval_function = eval_function
+        # Right now, only supporting 0 to 1
+        self.train_x = list(np.random.random_sample(bounds.shape[0])) 
+        for x in self.train_x:
+            self.train_y.append(self.eval_function(x))
 
-    ax_plot(observed_ax, observed_pred, 'Observed Values (Likelihood)')
-    return f
+    def run_bayes_opt(self, max_iters=100):
+        old_new_min = None
+        for _ in range(max_iters):
+            train_x_var = Variable(torch.Tensor(np.array(self.train_x)))
+            train_y_var = Variable(torch.Tensor(np.array(self.train_y)))
+            model = self.train_model(train_x_var, train_y_var)
+            new_min = find_minimum(model)
+            if new_min == old_new_min:
+                return new_min
+            old_new_min = new_min
+            self.train_x.append(new_min)
+            self.train_y.append(self.eval_function(new_min))
 
-def find_minimum(model):
-    test_x = Variable(torch.linspace(-5, 5, 51))
-    test_y = model(test_x)
-    lower, upper = test_y.confidence_region()
-    kappa = 100.0
-    mean, std = test_y.std(), test_y.mean()
-    boundary = mean + kappa * std
-    return test_x.data.numpy()[np.argmin(boundary.data.numpy())] 
-    # return test_x.data.numpy()[np.argmin(lower.data.numpy())]
+    def train_model(self, train_x, train_y):
+        model = ExactGPModel()
+        model.condition(train_x, train_y)
+        model.train()
+        optimizer = optim.Adam(model.parameters(), lr=0.1)
+        optimizer.n_iter = 0
+        for i in range(100):
+            optimizer.zero_grad()
+            output = model(train_x)
+            loss = -model.marginal_log_likelihood(output, train_y)
+            loss.backward()
+            optimizer.n_iter += 1
+            optimizer.step()
+        return model
 
+    def find_minimum(self, model):
+        test_x = Variable(torch.linspace(-5, 5, 51))
+        test_y = model(test_x)
+        lower, upper = test_y.confidence_region()
+        kappa = 100.0
+        mean, std = test_y.std(), test_y.mean()
+        boundary = mean + kappa * std
+        return test_x.data.numpy()[np.argmin(boundary.data.numpy())] 
 
+    def plot_model_and_predictions(self, model, train_x, train_y, plot_train_data=True):
+        f, observed_ax = plt.subplots(1, 1, figsize=(8, 8))
+        test_x = Variable(torch.linspace(-5, 5, 51))
+        observed_pred = model(test_x)
 
-def train_model(train_x, train_y, a, b, c, d, e, f):
-    model = ExactGPModel(a, b, c, d, e, f)
-    model.condition(train_x, train_y)
-    print("im here")
-    model.train()
-    print('now im here')
-    optimizer = optim.Adam(model.parameters(), lr=0.1)
-    optimizer.n_iter = 0
-    for i in range(50):
-        optimizer.zero_grad()
-        output = model(train_x)
-        loss = -model.marginal_log_likelihood(output, train_y)
-        loss.backward()
-        optimizer.n_iter += 1
-        # print('Iter %d/20 - Loss: %.3f   log_lengthscale: %.3f   log_noise: %.3f' % (
-        #     i + 1, loss.data[0],
-        #     model.covar_module.log_lengthscale.data[0, 0],
-        #     model.likelihood.log_noise.data[0]
-        # ))
-        optimizer.step()
-    return model
+        def ax_plot(ax, rand_var, title):
+            lower, upper = rand_var.confidence_region()
+            # if plot_train_data:
+            print (train_x, train_y)
+            ax.plot(train_x.data.numpy(), train_y.data.numpy(), 'k*')
+            ax.plot(test_x.data.numpy(), rand_var.mean().data.numpy(), 'b')
+            ax.fill_between(test_x.data.numpy(), lower.data.numpy(), upper.data.numpy(), alpha=0.5)
+            ax.set_ylim([-200, 200])
+            ax.legend(['Observed Data', 'Mean', 'Confidence'])
+            ax.set_title(title)
 
-
-def evaluate_model(model, train_x, train_y):
-    # Set back to eval mode
-    model.eval()
-    fig = plot_model_and_predictions(model, train_x, train_y)
-    plt.show()
-
-if __name__ == '__main__':
-    x_data = [-.25, .5]
-    for i in range(20):
-        print (x_data)
-        train_x = Variable(torch.Tensor(np.array(x_data)))
-        ### NOTE: Normally for bayesian optimizatoin I would not simply use a grid of values 
-        ### as the train_x every time. But I'm having trouble fitting this function, so I'm just using that
-        train_x = Variable(torch.linspace(-5, 5, 25))
-        # train_x = Variable(torch.linspace(0, 1, 11))
-        train_y = Variable(0.5*(train_x.data**4 - 16*train_x.data**2 + 5*train_x.data))
-        # train_y = Variable((torch.sin(train_x.data * (2 * math.pi)) + torch.randn(train_x.size()) * 0.2))
-        model = train_model(train_x, train_y, args.log_noise_min, args.log_noise_max, args.const_min, args.const_max, args.cov_min, args.cov_max)
-        print("THe model is ")
-        print(model(train_x).mean().data.numpy())
-        evaluate_model(model, train_x, train_y)
-
-        new_min = find_minimum(model)
-        x_data.append(new_min)
+        ax_plot(observed_ax, observed_pred, 'Observed Values (Likelihood)')
+        return f
---
+++ bo_fool.py
@@ -0,0 +1,74 @@
+import abc
+import adversaries
+import torch
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import bayesian_optimization
+
+
+
+class BOFool(adversaries.Adverarial_Base):
+
+    
+  def adversary_batch(self, data, model, target_class, image_reg, lr):
+    """Creates adversarial examples for one batch of data.
+
+    Helper function for create_one_adversary_batch.
+
+    Args:
+      data: images, labels tuple of batch to be altered
+      model: trained pytorch imagenet model
+      target_class: int, class to target for adverserial examples
+        If target_class is -1, optimize non targeted attack. Choose next closest class.
+      image_reg: Regularization constant for image loss component of loss function
+
+    Returns:
+      iters: Number of iterations it took to create adversarial example
+      MSE: Means Squared error between original and altered image.
+
+    """
+    # Load in first <batch_size> images for validation
+    images, original_labels =  data
+    np_imgs = images.data 
+    if self.cuda:
+      images = images.cuda()
+      original_labels = original_labels.cuda()
+    inputs = Variable(images, requires_grad = True)
+    outputs = model(inputs)
+    old_images = images.clone()
+    # Set target variables for model loss
+    new_labels = self.target_class_tensor(target_class, outputs, original_labels)
+
+    # Loop through each pimage, each channel, x, and y, perform bayesian optimazation
+    for img_num in range(np_imgs.shape[0]):
+    	np_img = np_imgs[img_num]
+		old_img = old_images[img_num]
+    	for x in range(np_img.shape[1]):
+    		for y in range(np_img.shape[2]):
+    			for c in range(np_img.shape[0]):
+				    def eval_function(new_val):
+				    	img_clone = np_img.clone()
+				    	img_clone[c, x, y] = new_val
+				    	var_img = Variable(torch.Tensor(img_clone).cuda())
+				    	img_pred = model(var_img)
+				    	loss = self.CE_MSE_loss(var_img, img_pred, old_img, new_labels[img_num], image_reg)
+				    	return loss
+				    bo = BayesOpt(eval_function)
+				    new_val = run_bayes_opt()
+				    # Very unsure about whether this change will carry over
+				    np_imgs[img_num, c, x, y] = new_val
+
+    model = self.make_eval_model()
+    inputsw = Variable(torch.Tensor(np_imgs).cuda())
+    outputs = model(inputs)
+    predicted_loss, predicted_classes = torch.max(outputs.data, 1)
+    max_diff = np.mean(((images - old_images).cpu().numpy().reshape(images.shape[0],-1).max(1)))
+    return 1, max_diff, self.percent_changed(original_labels, predicted_classes)
+
+
