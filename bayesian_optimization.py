92ba8c7109041aec1056f5f86354fed9a0b6c1af
Jared Frank <jaredfrank@dhcp-rhodes-1267.redrover.cornell.edu>
2017-10-25 19:25:51 -0400

    numpy clone is copy

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -54,6 +54,7 @@ class BayesOpt(object):
             old_new_min = new_min
             self.train_x.append(new_min)
             self.train_y += list(self.eval_function(new_min).data.cpu().numpy())
+        return new_min
 
     def train_model(self, train_x, train_y):
         model = ExactGPModel()
