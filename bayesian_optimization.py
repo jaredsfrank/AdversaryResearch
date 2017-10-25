b0f8260db68082a2e349bd10b57eceeddf7c9187
Jared Frank <jaredfrank@dhcp-rhodes-1267.redrover.cornell.edu>
2017-10-25 19:39:44 -0400

    numpy clone is copy

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -91,7 +91,7 @@ class BayesOpt(object):
         def ax_plot(ax, rand_var, title):
             lower, upper = rand_var.confidence_region()
             # if plot_train_data:
-            print (train_x, train_y)
+            print (train_x.data.cpu().numpy(), train_y.data.cpu().numpy())
             ax.plot(train_x.data.cpu().numpy(), train_y.data.cpu().numpy(), 'k*')
             ax.plot(test_x.data.numpy(), rand_var.mean().data.numpy(), 'b')
             ax.fill_between(test_x.data.numpy(), lower.data.numpy(), upper.data.numpy(), alpha=0.5)
