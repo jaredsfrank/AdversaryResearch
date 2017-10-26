750c102d55201c8f5eb9c5f9412ef331db9cb6fd
Jared Frank <jaredfrank@dhcp-rhodes-1267.redrover.cornell.edu>
2017-10-25 20:55:09 -0400

    numpy clone is copy

--- bo_fool.py
+++ bo_fool.py
@@ -73,7 +73,7 @@ class BOFool(adversaries.Adverarial_Base):
 
             var_img = Variable(torch.Tensor(np_imgs).cuda())
             # self.imshow(torchvision.utils.make_grid(var_img.data))
-            self.diff(torchvision.utils.make_grid(var_img.data), old_images)
+            self.diff(torchvision.utils.make_grid(var_img.data), torchvision.utils.make_grid(old_images))
             plt.show()
             img_pred = model(var_img)
             print(self.CE_MSE_loss(var_img, img_pred, old_img, new_labels[img_num], image_reg))
