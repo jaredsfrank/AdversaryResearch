613b9f823245fa4432e395d0bf97930f0f30de4d
Jared Frank <jaredfrank@dhcp-rhodes-1322.redrover.cornell.edu>
2017-11-28 20:36:12 -0500

    windowing

--- P_LBFGS.py
+++ P_LBFGS.py
@@ -32,7 +32,7 @@ class P_LBFGS(adversaries.Adverarial_Base):
   	print (images.shape[0])
   	mask = np.ones(images.shape)
   	mask[:,:,root_y::root_y+window_size, root_x::root_x+window_size] = 0
-  	print(mask)
+  	print(mask, mask[0,2,2])
   	images.masked_scatter_(torch.ByteTensor(mask), old_images)
   	return images
     
