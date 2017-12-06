57bd6f62bdfb1f6d9314e453ea803d16e1560bf8
Jared Frank <jaredfrank@dhcp-rhodes-1733.redrover.cornell.edu>
2017-12-06 12:04:46 -0500

    converting label name

--- convert_label.py
+++ convert_label.py
@@ -3,8 +3,8 @@ import pylab as plt
 
 
 def convert_label(image_num):
-	labels = np.loadtxt("imagenet_labels/imagenet_labels.csv", delimiter = ",", dtype = "string")
-	order = np.loadtxt("imagenet_labels/labels_list.csv", delimiter = ",", dtype = "string")
+	labels = np.loadtxt("imagenet_labels/imagenet_labels.csv", delimiter = ",", dtype = "str")
+	order = np.loadtxt("imagenet_labels/labels_list.csv", delimiter = ",", dtype = "str")
 	label = order[image_num-1]
 	print(labels[np.where(labels[:,0] == label)[0]])
 
