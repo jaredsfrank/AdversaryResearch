67b9c72ff579bf32866e5ea1e7b07ff0698b62db
Jared Frank <jaredfrank@Jareds-MacBook-Pro.local>
2017-12-19 11:32:19 -0500

    bigger batches

--- P_LBFGS.py
+++ P_LBFGS.py
@@ -170,7 +170,7 @@ class P_LBFGS(adversaries.Adverarial_Base):
           self.save_figure(old_images, "Before_{}_{}".format(image_reg, lr))
           plt.show()
         print((outputs.data[:, new_labels.data][:,0] - predicted_loss).cpu().numpy())
-        print(all_scores)
+        print(all_scores, type(all_scores), all_scores.dtype)
         np.savetxt("/scratch/jsf239/{}all_scores.csv".format(self.sub_dir), all_scores, delimiter = ",")
     return success, iters, max_diff, success
 
