d0fa7c26ee04258a2cc4a541e172a03fc67cdc4e
Jared Frank <jaredfrank@Jareds-MacBook-Pro.local>
2017-12-19 11:21:33 -0500

    bigger batches i think

--- P_LBFGS.py
+++ P_LBFGS.py
@@ -1,3 +1,6 @@
+# x = np.loadtxt(str(i).zfill(4)+"all_scores.csv", delimiter = ",")[:-1,:-1].T; x = np.repeat(np.repeat(x, 28, axis = 0), 28, axis = 1); plt.imshow(x, vmin = 0); plt.show()
+
+
 import abc
 import adversaries
 import torch
@@ -27,7 +30,7 @@ class P_LBFGS(adversaries.Adverarial_Base):
     else:
       return True
 
-  def window_image(self, old_images, images, root_x, root_y, window_size):
+  def window_image_old(self, old_images, images, root_x, root_y, window_size):
     """Resores all values in images besides window"""
     y_indices = np.tile(np.arange(window_size)+root_y, window_size)
     x_indices = np.repeat(np.arange(window_size)+root_x, window_size)
@@ -36,6 +39,19 @@ class P_LBFGS(adversaries.Adverarial_Base):
     mask[:,:,y_indices,x_indices] = 0
     images.masked_scatter_(mask, old_images)
     return images
+
+
+  def window_image(self, old_image, images, root_x, root_y, window_size):
+    """Resores all values in images besides window in place"""
+    y_indices = torch.LongTensor(np.tile(np.arange(window_size)+root_y, window_size)).cuda()
+    x_indices = torch.LongTensor(np.repeat(np.arange(window_size)+root_x, window_size)).cuda()
+    other_images = old_images.clone()
+    mask = torch.ByteTensor(images.shape)+1
+    if self.cuda:
+      mask = mask.cuda()
+    mask[:,:,:,:] = 1
+    other_images[:,:,y_indices,x_indices] = images[:,:,y_indices,x_indices] 
+    images[:] = other_images[:]
 
 
   def create_1000_batches(self, target_class, image_reg, lr):
@@ -97,7 +113,6 @@ class P_LBFGS(adversaries.Adverarial_Base):
       original_labels = original_labels.cuda()
     inputs = Variable(images, requires_grad = True)
     opt = optim.SGD(self.generator_hack(inputs), lr=lr, momentum=0.9)
-    print("learning rate is {}".format(lr))
     self.clamp_images(images)
     old_images = images.clone()
     outputs = model(inputs)
@@ -107,77 +122,54 @@ class P_LBFGS(adversaries.Adverarial_Base):
     print("The predicted classes are:")
     print(convert_label(predicted_classes.cpu().numpy()))
     print(convert_label(original_labels.cpu().numpy()))
-    iters = 0
+
     # Set target variables for model loss
     new_labels = self.target_class_tensor(target_class, outputs, original_labels)
-    all_scores = np.zeros(((images.shape[2]-WINDOW_SIZE)//(WINDOW_SIZE)+1, (images.shape[3]-WINDOW_SIZE)//(WINDOW_SIZE)+1))
-    prob_diffs = np.zeros(((images.shape[2]-WINDOW_SIZE)//(WINDOW_SIZE)+1, (images.shape[3]-WINDOW_SIZE)//(WINDOW_SIZE)+1))
+    all_scores = np.zeros((images.shape[0], (images.shape[2]-WINDOW_SIZE)//(WINDOW_SIZE)+1, (images.shape[3]-WINDOW_SIZE)//(WINDOW_SIZE)+1))
     success = 0
-    if not self.all_changed(original_labels, predicted_classes):
-      for root_x in range(0, images.shape[2]-WINDOW_SIZE, WINDOW_SIZE):
-        for root_y in range(0, images.shape[3]-WINDOW_SIZE, WINDOW_SIZE):
-          # root_x = 5
-          # root_y = 5
-          print("starting? {} {}".format(root_x, root_y))
-          images[:] = old_images[:]
-          iters = 0
-          predicted_classes = original_predictions
-          while self.check_iters(iters) and not self.all_changed(original_labels, predicted_classes):
-            if self.verbose and iters % 20 == 0:
-              print("Iteration {}".format(iters))
-            opt.zero_grad()
-            # Clamp loss so that all pixels are in valid range (Between 0 and 1 unnormalized)
-            self.clamp_images(images)
-            outputs = model(inputs)
-            # Compute full loss of adversarial example
-            loss = self.CE_MSE_loss(inputs, outputs, old_images, new_labels, image_reg)
-            predicted_loss, predicted_classes = torch.max(outputs.data, 1)
-            iters += 1
-            if self.verbose and iters % 20 == 0:
-              print("Target Class Weights Minus Predicted Weights of Original:")
-              print(outputs.data[:, new_labels.data][:,0] - predicted_loss)
-            
+    for root_x in range(0, images.shape[2]-WINDOW_SIZE, WINDOW_SIZE):
+      for root_y in range(0, images.shape[3]-WINDOW_SIZE, WINDOW_SIZE):
+        print("starting {} {}".format(root_x, root_y))
+        images[:] = old_images[:]
+        iters = 0
+        predicted_classes = original_predictions
 
-            y_indices = torch.LongTensor(np.tile(np.arange(WINDOW_SIZE)+root_y, WINDOW_SIZE)).cuda()
-            x_indices = torch.LongTensor(np.repeat(np.arange(WINDOW_SIZE)+root_x, WINDOW_SIZE)).cuda()
-            other_images = old_images.clone()
+        while self.check_iters(iters) and not self.all_changed(original_labels, predicted_classes):
+          if self.verbose and iters % 20 == 0:
+            print("Iteration {}".format(iters))
+          opt.zero_grad()
+          # Clamp loss so that all pixels are in valid range (Between 0 and 1 unnormalized)
+          self.clamp_images(images)
+          outputs = model(inputs)
+          # Compute full loss of adversarial example
+          loss = self.CE_MSE_loss(inputs, outputs, old_images, new_labels, image_reg)
+          predicted_loss, predicted_classes = torch.max(outputs.data, 1)
+          iters += 1
+          if self.verbose and iters % 20 == 0:
+            print("Target Class Weights Minus Predicted Weights of Original:")
+            print(outputs.data[:, new_labels.data][:,0] - predicted_loss)
+          
+          self.window_image(old_image, images, root_x, root_y, window_size)
 
-            mask = torch.ByteTensor(images.shape)+1
-            if self.cuda:
-              mask = mask.cuda()
-            mask[:,:,:,:] = 1
-            other_images[:,:,y_indices,x_indices] = images[:,:,y_indices,x_indices] 
-            images[:] = other_images[:]
-            outputs = model(inputs)
-            # Compute full loss of adversarial example
-            loss = self.CE_MSE_loss(inputs, outputs, old_images, new_labels, image_reg)
-            predicted_loss, predicted_classes = torch.max(outputs.data, 1)
+          outputs = model(inputs)
+          loss = self.CE_MSE_loss(inputs, outputs, old_images, new_labels, image_reg)
+          predicted_loss, predicted_classes = torch.max(outputs.data, 1)
+          loss.backward()
+          opt.step()
+          new_labels = self.target_class_tensor(target_class, outputs, original_labels)
+          all_scores[:, root_x//(WINDOW_SIZE), root_y//(WINDOW_SIZE)] += (original_labels.cpu().numpy() == predictions.cpu().numpy())
 
+        if iters < self.max_iters:
+          success = 1
+        success += len(np.where(all_scores[:, root_x//(WINDOW_SIZE), root_y//(WINDOW_SIZE)]<self.max_iters)[0])
+        max_diff = np.mean(((images - old_images).cpu().numpy().reshape(images.shape[0],-1).max(1)))
+        print("Max diff was {}, iters was {}".format(max_diff, iters))
 
-            if self.verbose and iters % 20 == 0:
-              print("Target Class Weights Minus Predicted Weights of Cropped:")
-              print(outputs.data[:, new_labels.data][:,0] - predicted_loss)
-            else:
-                loss.backward()
-                opt.step()
-                # inputs.data -= lr*torch.sign(inputs.grad).data
-                new_labels = self.target_class_tensor(target_class, outputs, original_labels)
-                # self.MSE(images, Variable(old_images))
-          if iters < self.max_iters:
-            success = 1
-          max_diff = np.mean(((images - old_images).cpu().numpy().reshape(images.shape[0],-1).max(1)))
-          print("Max diff was {}, iters was {}".format(max_diff, iters))
-          if self.show_images:
-            self.save_figure(inputs.data, "After_{}_{}".format(image_reg, lr))
-            self.save_figure(old_images, "Before_{}_{}".format(image_reg, lr))
-            self.diff(images, old_images)
-            plt.show()
-          all_scores[root_x//(WINDOW_SIZE), root_y//(WINDOW_SIZE)] = iters
-          print((outputs.data[:, new_labels.data][:,0] - predicted_loss).cpu().numpy())
-          prob_diffs[root_x//(WINDOW_SIZE), root_y//(WINDOW_SIZE)] = (outputs.data[:, new_labels.data][:,0] - predicted_loss).cpu().numpy()
-          np.savetxt("/scratch/jsf239/{}all_scores.csv".format(self.sub_dir), all_scores, delimiter = ",", fmt = "%d")
-          np.savetxt("/scratch/jsf239/{}prob_diffs.csv".format(self.sub_dir), prob_diffs, delimiter = ",")
-    else:
-      return 0, 0, 0, 1
+        if self.show_images:
+          self.save_figure(inputs.data, "After_{}_{}".format(image_reg, lr))
+          self.save_figure(old_images, "Before_{}_{}".format(image_reg, lr))
+          plt.show()
+        print((outputs.data[:, new_labels.data][:,0] - predicted_loss).cpu().numpy())
+        np.savetxt("/scratch/jsf239/{}all_scores.csv".format(self.sub_dir), all_scores, delimiter = ",", fmt = "%d")
     return success, iters, max_diff, success
 
